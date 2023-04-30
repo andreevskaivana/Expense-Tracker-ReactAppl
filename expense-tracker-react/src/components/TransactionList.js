96ea5eedf0b1fb37243a009dc31508cbdaca9c0f
andreevskaivana <ivana.andreevska25@gmail.com>
2023-04-30 03:36:26 +0200

    Initial React commit and first components

---
+++ expense-tracker-react/src/components/TransactionList.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function TransactionList() {
+  return (
+    <>
+    <h3>History</h3>
+      <ul className="list">
+       
+      </ul>
+    </>
+
+    )
+}
